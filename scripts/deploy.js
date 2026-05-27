e0e35b93738fe167362604a571d120d8804753a3
Bah Dieudonne <bahdieudonne49@gmail.com>
2026-05-27 17:55:34 +0100

    Session two

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+import hre from "hardhat";
+
+const { ethers } = await hre.network.connect();
+
+async function main() {
+    const Storage = await ethers.getContractFactory("SimpleStorage");
+
+    const contract = await Storage.deploy();
+
+    await contract.waitForDeployment();
+
+    console.log("Contract deployed to:", await contract.getAddress());
+}
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+}); 
